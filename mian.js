122595b018a372780c0e089e714823fd739cf9e8
mohamed_elsheshtawey <114621103+Elsheshtawey1@users.noreply.github.com>
2024-09-19 23:21:50 +0300

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,13 @@
+ window.onscroll = function() {
+        const button = document.getElementById('scrollButton');
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            button.style.display = "block";
+        } else {
+            button.style.display = "none";
+        }
+    };
+
+    
+    function upScroll() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }
